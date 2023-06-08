198df52639d4a5124997daecf41ca773007e76a8
smussbacher <simon.mussbacher@student.tugraz.at>
2023-06-08 15:26:11 +0200

    reworked Task1 works now

--- task1.py
+++ task1.py
@@ -1,8 +1,11 @@
+from aalpy.SULs import MealySUL
+from aalpy.learning_algs import run_Lstar
+from aalpy.oracles import RandomWordEqOracle
+from random import seed, choices, randint
 import dill as pickle
 from abc import ABC, abstractmethod
-import random
 
-# interface that all loaded implementations implement
+
 class AbstractVendingMachine(ABC):
 
     @abstractmethod
@@ -19,7 +22,36 @@ class AbstractVendingMachine(ABC):
     @abstractmethod
     def reset(self):
         pass
+    
+class VendingMachineSUL(MealySUL):
+    def __init__(self, vending_machine):
+        super().__init__(None)  # Call MealySUL constructor without providing a Mealy Machine
+        self.vending_machine = vending_machine
+        self.reset()
+        self.coin_count = 0
 
+    def step(self, letter):
+        if letter in {0.5, 1.0, 2.0}:
+            self.coin_count += 1
+            if self.coin_count > 5:
+                self.coin_count = 5
+            return self.vending_machine.add_coin(letter)
+        else: 
+            output = self.vending_machine.push_button(letter)
+            self.coin_count = 0
+            return output
+
+    def get_input_alphabet(self):
+        return [0.5, 1.0, 2.0, 'coke', 'peanuts', 'water']
+    
+    def pre(self):
+        self.reset()
+
+    def post(self):
+        pass
+
+    def reset(self):
+        self.vending_machine.reset()
 
 if __name__ == '__main__':
 
@@ -27,19 +59,45 @@ if __name__ == '__main__':
     black_box_models = []
     for model_pickle in ['vending_machine_dorian.pickle', 'vending_machine_edi.pickle',
                          'vending_machine_moritz.pickle', 'vending_machine_matthias.pickle']:
-
         with open(f'black_box_impl/{model_pickle}', 'rb') as handle:
             loaded_model = pickle.load(handle)
             black_box_models.append(loaded_model)
+    seed(1)
+    learned_models = []
+    name = ['dorian', 'edi', 'moritz', 'matthias']
+    cnt = 0
+    # Wrap each vending machine model in a SUL and learn a Mealy machine model
+    for vending_machine in black_box_models:
+        sul = VendingMachineSUL(vending_machine)
+        input_alphabet = sul.get_input_alphabet()
+        eq_oracle = RandomWordEqOracle(input_alphabet, sul, num_walks=100, min_walk_len=3, max_walk_len=6)
+        learned_model = run_Lstar(input_alphabet, sul, eq_oracle, automaton_type='mealy', print_level=3)
+        learned_model.visualize(path='Learned_model_' + name[cnt] + '.pdf')
+        learned_models.append(learned_model)
+        cnt += 1
 
-    # Example how to interact with the vending machine
-    # get on of the vending machines
-    vm = black_box_models[3]
-    # reset it
-    vm.reset()
-    # perform an action
-    print(vm.push_button('coke'))
-    print(vm.add_coin(1))
+    # TODO 2. Write a function that performs learning-based testing to find the differences between them (if any)
+    def compare_models(model1, model2, input_alphabet, num_tests=1000):
+        differences = []
+        for _ in range(num_tests):
+            inputs = choices(input_alphabet, k=randint(8, 8))
+            outputs1 = model1.execute_sequence(model1.initial_state, inputs)
+            outputs2 = model2.execute_sequence(model2.initial_state, inputs)
+            if outputs1 != outputs2:
+                differences.append((inputs, outputs1, outputs2))
+        return differences
+    # Compare all pairs of models
+    for i in range(len(learned_models)):
+        for j in range(i + 1, len(learned_models)):
+            differences = compare_models(learned_models[i], learned_models[j], input_alphabet)
+            print(f"Differences between model {i} and model {j}:")
+            for inputs, outputs1, outputs2 in differences:
+                print(f"Inputs: ")
+                print(f"{inputs}")
+                print(f"Outputs for model {i}:")
+                print(f"{outputs1}")
+                print(f"Outputs for model {j}:")
+                print(f"{outputs2}")
 
     # TODOs
     # TODO 1. Learn models of Vending Machines
@@ -47,73 +105,5 @@ if __name__ == '__main__':
     # TODO 3. By performing an analysis of the learned models with function from TODO 2,
     #  figure out which model is correct, and what are the bugs in other models
     # TODO 4. Report the findings in the Report.md
+    # Define the possible actions
     
-    # TODO 1
-    def learn_model(vm, actions, num_trials=1000):
-        """
-        This function learns the behavior of a vending machine model by performing random sequences of actions
-        and recording the results.
-        """
-        model_behavior = {}
-
-        for _ in range(num_trials):
-            sequence = random.choices(actions, k=random.randint(1, 10))  # Random sequence of actions
-            sequence_str = ', '.join(map(str, sequence))
-            vm.reset()
-            output = []
-            for action in sequence:
-                if isinstance(action, float):  # If the action is adding a coin
-                    output.append(vm.add_coin(action))
-                else:  # If the action is pressing a button
-                    output.append(vm.push_button(action))
-            model_behavior[sequence_str] = output
-
-        return model_behavior
-
-
-    # TODO 2
-    def learning_based_testing(vm1, vm2, actions, num_trials=1000):
-        """
-        This function performs learning-based testing on two vending machine models by executing the same sequence of
-        actions on both and checking if their outputs differ.
-        """
-        differences = {}
-
-        for _ in range(num_trials):
-            sequence = random.choices(actions, k=random.randint(1, 10))  # Random sequence of actions
-            sequence_str = ', '.join(map(str, sequence))
-            vm1.reset()
-            vm2.reset()
-            output1 = []
-            output2 = []
-            for action in sequence:
-                if isinstance(action, float):  # If the action is adding a coin
-                    output1.append(vm1.add_coin(action))
-                    output2.append(vm2.add_coin(action))
-                else:  # If the action is pressing a button
-                    output1.append(vm1.push_button(action))
-                    output2.append(vm2.push_button(action))
-            if output1 != output2:
-                differences[sequence_str] = (output1, output2)
-
-        return differences
-
-
-        # Defining the possible actions
-        actions = [0.5, 1, 2, 'coke', 'peanuts', 'water']
-
-        # Learning the behavior of each vending machine
-        vending_machine_behaviors = [learn_model(vm, actions) for vm in black_box_models]
-
-        # Comparing the behavior of each pair of vending machines
-        for i in range(len(black_box_models)):
-            for j in range(i + 1, len(black_box_models)):
-                print("---------------------------------------------------")
-                print(f"Differences between model {i} and model {j}:")
-                differences = learning_based_testing(black_box_models[i], black_box_models[j], actions)
-                for sequence_str, outputs in differences.items():
-                    print(f"For sequence: {sequence_str}")
-                    print(f"Model {i} output:")
-                    print(outputs[0])
-                    print(f"Model {j} output:")
-                    print(outputs[1])
